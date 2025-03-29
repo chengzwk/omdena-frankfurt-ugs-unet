49d0dd37d7ce97a0354665b44bbc97cad0bd98c3
Cheng Zhen <cheng.zhenwk@gmail.com>
2025-03-29 17:34:20 +0100

    Add code for making prediction on Frankfurt dataset

---
+++ frankfurt_data_preparation.py
@@ -0,0 +1,344 @@
+# Import packages
+import os
+import sys
+import rasterio
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.gridspec as gridspec
+from natsort import natsorted
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+
+def print_progress_bar(iteration, total, length=40):
+    """Print a progress bar."""
+    percent = (iteration / total) * 100
+    filled_length = int(length * iteration // total)
+    bar = '█' * filled_length + '-' * (length - filled_length)
+    sys.stdout.write(f'\r|{bar}| {percent:.2f}%')
+    sys.stdout.flush()
+
+def normalize_by_layer(image_array):
+    """
+    Function to normalize image data to the same max(1) and min(0)
+    Since different layers(bands) have different scales, normalization will be done layer by layer
+    """
+    # Normalize by band
+    image_array = image_array.astype(np.float64)  # Convert dtype of image file from int to float64
+
+    for i in range(image_array.shape[2]):
+        layer_min = np.min(image_array[:, :, i])
+        layer_max = np.max(image_array[:, :, i])
+
+        try:
+            image_array[:, :, i] = (image_array[:, :, i] - layer_min) / (layer_max - layer_min)
+        except ZeroDivisionError:
+            print(f"Band {i} has zero variation (min = max = {layer_min}). Skipping normalization.")
+            image_array[:, :, i] = 0  # Set the band to default value 0
+
+    return image_array
+
+def convert_binary_mask(mask_array, multiclass=True, threshold=0.5):
+    """
+    Convert fractional mask to binary mask
+    By default, convert it to multiclass mask of 5 classes
+    Class numbers:
+    0: Impervious surfaces and buildings
+    1: Low vegetation
+    2: Trees
+    3: Water
+    4: Clutter/Background
+    """
+    if multiclass:
+        mask_array = np.argmax(mask_array, axis=2, keepdims=True)
+    else:
+        mask_array = ((mask_array[:, :, 1] + mask_array[:, :, 2]) >= threshold).astype(np.uint8)
+        mask_array = mask_array[:, :, np.newaxis]
+    return mask_array
+
+def cropping_coordinates(h, w, crop_size):
+    """
+    Calculate cropping coordinates of image file
+    """
+    top = (h - crop_size) // 2
+    left = (w - crop_size) // 2
+    bottom = top + crop_size
+    right = left + crop_size
+
+    return top, left, bottom, right
+
+def crop_image(image, crop_size=128):
+    """Crop the image and mask to 128x128 pixels from the center."""
+    if image.shape[1:] != (crop_size, crop_size):
+        h, w = image.shape[1:]  # Get height and width from the image dimensions
+        top, left, bottom, right = cropping_coordinates(h, w, crop_size)
+        cropped_image = image[:, top:bottom, left:right]  # Keep all bands
+    return cropped_image
+
+def read_file(data_dir, image_dir, mask_dir, multiclass=True, threshold=0.5, subset_size=None, crop_size=128):
+    """
+    Read satellite image files and corresponding masks as numpy arrays,
+    normalize image array to the same scale by band, and convert fractional masks into binary masks.
+    Read subset_size image files for training
+    Note we're only using images from the same time (files ends in 1_GeoTIFF)
+    """
+    image_dataset = []
+    mask_dataset = []
+    # mask_dir = image_dir + '_masks'
+    image_files = [f for f in os.listdir(os.path.join(data_dir, image_dir)) if f.endswith('.tif')]
+    image_files = natsorted(image_files)
+    mask_files = [f for f in os.listdir(os.path.join(data_dir, mask_dir)) if f.endswith('.tif')]
+    mask_files = natsorted(mask_files)
+    if subset_size is not None:
+        image_files = image_files[:subset_size]
+    total_files = len(image_files)
+
+    # Print progress
+    print(f"Reading images and masks from {image_dir} and {mask_dir}")
+
+    for i, image_file in enumerate(image_files):
+        # mask_file = image_file.replace('.tif', '_fractional_mask.tif')
+        mask_file = mask_files[i]
+        image_path = os.path.join(data_dir, image_dir, image_file)
+        mask_path = os.path.join(data_dir, mask_dir, mask_file)
+
+        # Read image file
+        with (rasterio.open(image_path) as img):
+            image_array = img.read()
+        image_array = crop_image(image_array)
+        image_array = np.transpose(image_array, [1, 2, 0])  # move the axis for bands to the third axis
+        image_array[np.isnan(image_array)] = 0                   # replace nan with 0
+        image_array = image_array[:, :, (1, 2, 3)]               # get the bands you want; (1, 2, 3) is RGB bands
+        image_array = normalize_by_layer(image_array)            # Normalize the image by band
+
+        # Read mask file
+        with rasterio.open(mask_path) as msk:
+            mask_array = msk.read()
+        mask_array = crop_image(mask_array)
+        mask_array = np.transpose(mask_array, [1, 2, 0])          # move the axis for bands to the third axis
+        mask_array[np.isnan(mask_array)] = 0                           # replace nan with 0
+        # mask_array = convert_binary_mask(mask_array, multiclass=multiclass, threshold=threshold)  # Convert fractional mask to binary
+        mask_array = normalize_by_layer(mask_array)
+
+        image_dataset.append(image_array)
+        mask_dataset.append(mask_array)
+
+        # Print progress
+        print_progress_bar(i + 1, total_files)  # Update progress bar
+
+    image_dataset = np.array(image_dataset)
+    mask_dataset = np.array(mask_dataset)
+    mask_dataset = mask_dataset.astype(np.float64)  # Convert dtype of mask file from int to float64
+
+    # Print and log progress
+    print("\nFinished reading images")
+
+    return image_dataset, mask_dataset
+
+def remove_images(image_dataset, mask_dataset, threshold):
+    """
+    This function remove images and corresponding masks with high proportion of backgrounds
+    Definition of backgrounds:
+        - urban green space: classes 3 and 4
+        - backgrounds: classes 2, 5, and 6
+    Input:
+        - image_dataset, mask_dataset: numpy array of satellite images and corresponding masks
+        - threshold: if proportion of backgrounds is higher than threshold in the image, the image will be removed
+    Output:
+        - balanced array without those chips
+    """
+    # Get the id of images to remove
+    id_to_remove = []
+
+    for i in range(len(mask_dataset)):
+        mask = mask_dataset[i, :, :, 0]
+        tot_pixel = mask.size
+        background_pixel = np.count_nonzero(np.isin(mask, [2, 5, 6]))
+        if background_pixel > tot_pixel * threshold:
+            id_to_remove.append(i)
+
+    # Get the balanced dataset
+    image_dataset_balanced = []
+    mask_dataset_balanced = []
+
+    for i in range(len(image_dataset)):
+        if not(i in id_to_remove):
+            image = image_dataset[i]
+            mask = mask_dataset[i]
+            image_dataset_balanced.append(image)
+            mask_dataset_balanced.append(mask)
+
+    image_dataset_balanced = np.array(image_dataset_balanced)
+    mask_dataset_balanced = np.array(mask_dataset_balanced)
+
+    return image_dataset_balanced, mask_dataset_balanced
+
+def show_statistics(image_dataset, mask_dataset):
+    print("Image data shape is: ", image_dataset.shape)
+    print("Mask data shape is: ", mask_dataset.shape)
+    print("Max pixel value in image is: ", image_dataset.max())
+    print("Min pixel value in image is: ", image_dataset.min())
+    print("Labels in the mask are : ", np.unique(mask_dataset))
+
+    multiclass = len(np.unique(mask_dataset)) > 2
+    total_pixels = mask_dataset.size  # Get total number of pixels in the dataset
+
+    if multiclass:
+        for i in range(5):
+            count = np.count_nonzero(mask_dataset == i)
+            percentage = (count / total_pixels) * 100
+            print(f"Number of pixels in class {i}: {count} ({percentage:.2f}%)")
+
+        background_count = np.count_nonzero(np.isin(mask_dataset, [0, 3, 4]))
+        background_percentage = (background_count / total_pixels) * 100
+        print(f"Number of background pixels: {background_count} ({background_percentage:.2f}%)")
+
+        low_veg_count = np.count_nonzero(mask_dataset == 1)
+        low_veg_percentage = (low_veg_count / total_pixels) * 100
+        print(f"Number of low vegetation pixels: {low_veg_count} ({low_veg_percentage:.2f}%)")
+
+        tree_count = np.count_nonzero(mask_dataset == 2)
+        tree_percentage = (tree_count / total_pixels) * 100
+        print(f"Number of tree pixels: {tree_count} ({tree_percentage:.2f}%)")
+
+    else:
+        non_veg_count = np.count_nonzero(mask_dataset == 0)
+        non_veg_percentage = (non_veg_count / total_pixels) * 100
+        print(f"Number of non-vegetation pixels: {non_veg_count} ({non_veg_percentage:.2f}%)")
+
+        veg_count = np.count_nonzero(mask_dataset == 1)
+        veg_percentage = (veg_count / total_pixels) * 100
+        print(f"Number of vegetation pixels: {veg_count} ({veg_percentage:.2f}%)")
+
+def inspect_dataset(image_dataset, mask_dataset, sample_size=2):
+    """
+    Randomly select and plot sample_size images and corresponding binary masks for visual inspection
+    """
+    # Randomly select sample_size files
+    samples = random.sample(list(np.arange(len(image_dataset))), min(sample_size, len(image_dataset)))
+    sample_images = [image_dataset[i, :, :, :] for i in samples]
+    sample_masks = [mask_dataset[i, :, :, :] for i in samples]
+    multiclass = len(np.unique(mask_dataset)) > 2
+
+    # Display the randomly selected images and masks
+    for i in range(len(sample_images)):
+        if multiclass:
+            class_labels = {
+                0: "Class 1&2: impervious",
+                1: "Class 3: low veg",
+                2: "Class 4: trees",
+                3: "Class 5: water",
+                4: "Class 6: clutter"}
+
+            class_colors = {
+                0: "#808080",  # Gray
+                1: "#ADFF2F",  # Light Green
+                2: "#006400",  # Dark Green
+                3: "#1E90FF",  # Blue
+                4: "#8B4513",  # Brown
+            }
+
+            cmap = plt.cm.colors.ListedColormap([class_colors[c] for c in sorted(class_colors)])
+            norm = plt.cm.colors.BoundaryNorm(boundaries=[-0.5, 0.5, 1.5, 2.5, 3.5, 4.5],
+                                              ncolors=5)  # Use a discrete colormap
+        else:
+            class_labels = {
+                0: "Class 0: non-vegetation",
+                1: "Class 1: vegetation",
+            }
+
+            class_colors = {
+                0: "#808080",  # Gray
+                1: "#006400",  # Dark Green
+            }
+
+            cmap = plt.cm.colors.ListedColormap([class_colors[c] for c in sorted(class_colors)])
+            norm = plt.cm.colors.BoundaryNorm(boundaries=[-0.5, 0.5, 1.5],
+                                              ncolors=2)  # Use a discrete colormap
+
+        fig = plt.figure(figsize=(12, 5))
+        gs = gridspec.GridSpec(1, 3, width_ratios=[1, 1, 0.6])  # Adjust width ratios
+
+        ax0 = fig.add_subplot(gs[0])  # Satellite image
+        ax1 = fig.add_subplot(gs[1])  # Mask
+        ax_legend = fig.add_subplot(gs[2])  # Legend axes
+
+        # Plot satellite image
+        ax0.imshow(sample_images[i][:, :, :3])
+        ax0.set_title("Satellite Image")
+        ax0.axis("off")
+        # Plot mask
+        ax1.imshow(sample_masks[i], cmap=cmap, norm=norm)
+        ax1.set_title("Mask")
+        ax1.axis("off")
+
+        # Create legend (same as before)
+        handles = [mpatches.Patch(color=class_colors[key], label=class_labels[key]) for key in sorted(class_labels)]
+        ax_legend.legend(handles=handles, loc='center left', title='Legend')
+        ax_legend.axis('off')  # Hide legend axes ticks and labels
+
+        # Manually adjust subplot parameters for better spacing
+        gs.update(wspace=0.05, hspace=0.05)  # Adjust spacing here
+        plt.show()
+
+def split_dataset(image_dataset, mask_dataset, test_size=0.15, val_size=0.15, random_state=42):
+    """Split dataset into train, validation, and test sets."""
+
+    # First, split off the test set
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        image_dataset, mask_dataset, test_size=test_size, random_state=random_state
+    )
+
+    # Then, split train_val into actual train and validation sets
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train_val, y_train_val, test_size=val_size / (1 - test_size), random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def categorical_mask_dataset(y_train, num_classes=2):
+    """Turns mask dataset (y_train, y_val, y_test) into categorical (one-hot encoded) format."""
+    y_train_cat = to_categorical(y_train, num_classes=num_classes)
+    return y_train_cat
+
+def formatted_print_shapes(X_train, X_val, X_test, y_train, y_val, y_test):
+    """
+    Prints the shapes of training, validation, and test data in a formatted way.
+    Also prints steps per epoch and validation steps if provided.
+    """
+    print("Data Shapes:")
+    print("-" * 20)
+    print(f"X_train: {X_train.shape}")
+    print(f"X_val:   {X_val.shape}")
+    print(f"X_test:  {X_test.shape}")
+    print("-" * 20)
+    print(f"y_train: {y_train.shape}")
+    print(f"y_val:   {y_val.shape}")
+    print(f"y_test:  {y_test.shape}")
+    print("-" * 20)
+
+
+if __name__ == "__main__":
+    # Define paths
+    data_dir = os.path.expanduser("~/Documents/Omdena/Frankfurt-dataset")
+
+    # Read image and mask files from selected dataset
+    # image_dirs = sorted([d.replace('_masks', '') for d in os.listdir(data_dir) if d.endswith('_masks')])
+    image_dir = '01_Sentinel-2_medianImage_April-June_2021'
+    mask_dir = '02_Binary_FFM_downscaled_10m'
+    # subset_size = 100
+    image_dataset, mask_dataset = read_file(data_dir, image_dir, mask_dir, multiclass=False)
+
+    # Print statistics of the dataset and visually inspect the dataset
+    show_statistics(image_dataset, mask_dataset)
+    inspect_dataset(image_dataset, mask_dataset, sample_size=13)
+
+    # Get the balanced dataset
+    # image_dataset_balanced, mask_dataset_balanced = remove_images(image_dataset, mask_dataset, 0.86)
+    # del image_dataset, mask_dataset
+    # print(image_dataset_balanced.shape, mask_dataset_balanced.shape)
+
+
+
---
+++ frankfurt_model_prediction.py
@@ -0,0 +1,173 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.metrics import IoU, MeanIoU
+from sklearn.metrics import roc_curve, auc
+
+from frankfurt_data_preparation import read_file, show_statistics, inspect_dataset
+
+
+def calculate_f1_score(precision, recall):
+    """Calculates the F1 score and avoids division by zero."""
+    denominator = precision + recall
+    if denominator == 0:
+        f1_score = 0.0
+    else:
+        f1_score = 2 * precision * recall / (precision + recall)
+    return f1_score
+
+def predict(model, X_test, threshold=0.5):
+    """Generate predictions and apply thresholding."""
+    y_pred = model.predict(X_test)
+    # y_pred = model(X_test, training=True)
+    y_pred = tf.sigmoid(y_pred).numpy()
+    y_pred_thresholded = (y_pred >= threshold).astype(int)
+
+    return y_pred, y_pred_thresholded
+
+def evaluate_model(model, X_test, y_test):
+    """Evaluate model on test set and return loss, accuracy, precision, and recall."""
+    loss, acc, precision, recall, mean_iou, iou_class1 = model.evaluate(X_test, y_test, verbose=0)
+    f1_score = calculate_f1_score(precision, recall)
+
+    print(f"Loss: {loss:.4f}")
+    print(f"Accuracy: {acc:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"F1 Score: {f1_score:.4f}")
+    print(f"Mean IoU: {mean_iou:.4f}")
+    print(f"IoU for class 1: {iou_class1:.4f}")
+
+    return loss, acc, precision, recall, f1_score
+
+def compute_iou(y_pred_thresholded, y_test):
+    """Compute IoU for class 1 and Mean IoU."""
+    iou_metric = IoU(num_classes = 2, target_class_ids=[1])
+    mean_iou_metric = MeanIoU(num_classes = 2)
+
+    iou_metric.update_state(y_pred_thresholded, y_test)
+    mean_iou_metric.update_state(y_pred_thresholded, y_test)
+
+    iou_value = iou_metric.result().numpy()
+    mean_iou_value = mean_iou_metric.result().numpy()
+
+    print(f"IoU for class 1: {iou_value:.4f}")
+    print(f"Mean IoU: {mean_iou_value:.4f}")
+
+    return iou_value, mean_iou_value
+
+def plot_roc_curve(y_pred_thresholded, y_test, result_dir):
+    """Plot ROC curve and compute AUC."""
+    y_test_raveled = y_test.ravel().astype(int)
+    y_pred_thresholded_raveled = y_pred_thresholded.ravel().astype(int)
+
+    fpr, tpr, thresholds = roc_curve(y_test_raveled, y_pred_thresholded_raveled)
+    auc_value = auc(fpr, tpr)
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(fpr, tpr, label=f"AUC = {auc_value:.4f}")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curve")
+    plt.legend(loc="lower right")
+    plt.grid()
+    plt.savefig(os.path.join(result_dir, "ROC_curve.png"))
+
+    print(f"AUC: {auc_value:.4f}")
+    return auc_value
+
+def visualize_predictions(X, y_true, y_pred, result_dir, title="Predictions vs. Ground Truth"):
+    """Helper function to visualize predictions against ground truth."""
+    num_samples = X.shape[0]
+    fig, axes = plt.subplots(num_samples, 3, figsize=(10, 5 * num_samples))
+
+    if num_samples == 1:
+        axes = np.expand_dims(axes, axis=0)  # Ensure consistent indexing
+
+    for i in range(num_samples):
+        axes[i, 0].imshow(X[i])
+        axes[i, 0].set_title("Input Image")
+        axes[i, 0].axis("off")
+
+        axes[i, 1].imshow(y_true[i].squeeze(), cmap="gray")
+        axes[i, 1].set_title("Ground Truth Mask")
+        axes[i, 1].axis("off")
+
+        axes[i, 2].imshow(y_pred[i].squeeze(), cmap="gray")
+        axes[i, 2].set_title("Predicted Mask")
+        axes[i, 2].axis("off")
+
+    plt.suptitle(title, fontsize=14)
+    plt.tight_layout()
+    plt.savefig(os.path.join(result_dir, f"{title}.png"))
+    plt.close()
+
+def run_evaluation(model, X_test, y_test, result_dir):
+    """Run all evaluation steps."""
+    print("Model Evaluation")
+    print("-" * 20)
+
+    # Prediction
+    y_pred, y_pred_thresholded = predict(model, X_test)
+
+    # Evaluate model performance
+    evaluate_model(model, X_test, y_test)
+
+    # Compute IoU
+    # compute_iou(y_pred_thresholded, y_test)
+
+    # Plot ROC curve and compute AUC
+    plot_roc_curve(y_pred_thresholded, y_test, result_dir)
+
+    # Visualize predictions vs. ground truth
+    visualize_predictions(X_test, y_test, y_pred_thresholded, result_dir, title="Final Predictions")
+
+
+if __name__ == "__main__":
+    # --- Load test data ---
+    # Define paths
+    data_dir = os.path.expanduser("~/Documents/Omdena/Frankfurt-dataset")
+
+    # Read image and mask files from selected dataset
+    # image_dirs = sorted([d.replace('_masks', '') for d in os.listdir(data_dir) if d.endswith('_masks')])
+    image_dir = '01_Sentinel-2_medianImage_April-June_2021'
+    mask_dir = '02_Binary_FFM_downscaled_10m'
+    # subset_size = 100
+    image_dataset, mask_dataset = read_file(data_dir, image_dir, mask_dir, multiclass=False)
+
+    # Print statistics of the dataset and visually inspect the dataset
+    show_statistics(image_dataset, mask_dataset)
+    inspect_dataset(image_dataset, mask_dataset)
+
+    X_test = image_dataset
+    y_test = mask_dataset
+
+    # --- Load model and compile ---
+    result_dir = os.path.expanduser("~/Documents/Omdena/omdena-frankfurt-ugs-unet/experiment08_decrease_weight_decay")
+    model = tf.keras.models.load_model(os.path.join(result_dir, 'unet.keras'), compile=False)  # Load trained model
+
+    # Recompile with the correct metrics
+    model.compile(
+        optimizer=tf.keras.optimizers.AdamW(learning_rate=1e-4, weight_decay=5e-5),
+        loss=tf.keras.losses.BinaryFocalCrossentropy(from_logits=True),
+        metrics=[
+            "accuracy",
+            tf.keras.metrics.Precision(thresholds=0),
+            tf.keras.metrics.Recall(thresholds=0),
+            tf.keras.metrics.BinaryIoU(target_class_ids=[0, 1], threshold=0.0),
+            tf.keras.metrics.BinaryIoU(target_class_ids=[1], threshold=0.0)
+        ]
+    )
+
+    # --- Evaluate model on test set ---
+    result_dir_eval = 'frankfurt_predictions'
+    os.makedirs(result_dir_eval, exist_ok=True)
+    run_evaluation(model, X_test, y_test, result_dir_eval)
+
+
+
+
+
+
+
